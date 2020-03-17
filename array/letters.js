1def7458d3a75e2af265d10f86e2bf70c9fb86c2
JustCallMeJoJo <59923599+JustCallMeJoJo@users.noreply.github.com>
2020-03-17 08:28:23 +0100

    Update letters.js

--- array/letters.js
+++ array/letters.js
@@ -1,1 +1,13 @@
-
+var anyLetter = [];
+function add(letter) {
+  anyLetter.push(letter);
+}
+add("J");
+add("o");
+add("J");
+add("o");
+function convertStr(anyLetter, item) {
+  return anyLetter + item;
+}
+var converted = anyLetter.reduce(convertStr);
+console.log(converted);
