fa0f0a5e3b8ef787245ababceb5fca5f029371b5
JustCallMeJoJo <59923599+JustCallMeJoJo@users.noreply.github.com>
2020-03-17 08:24:29 +0100

    Update grades.js

--- array/grades.js
+++ array/grades.js
@@ -1,1 +1,15 @@
-
+let votesAlumn = {
+    votes: [],
+    add(vote) {
+        this.votes.push(vote)
+    },
+    average () {
+        return this.votes.reduce((acc, corr) => acc + corr)/this.votes.length
+    }
+}
+votesAlumn.add(6);
+votesAlumn.add(8);
+votesAlumn.add(7);
+votesAlumn.add(9);
+console.log (votesAlumn.votes);
+console.log (votesAlumn.average());
